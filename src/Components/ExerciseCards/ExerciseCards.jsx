017156cf26a1040429f26de8a85049acd6c7481c
Rylen Lobo <loborylen98334@gmail.com>
2023-09-01 22:29:02 +0530

    test

---
+++ src/Components/ExerciseCards/ExerciseCards.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import  "./ExerciseCards.css"
+
+const ExerciseCards = ({ bodyPart, equipment, gifUrl, id, name, target }) => {
+  return (
+    <>
+      <div className="card">
+        <p className="ex-name" id={id}>
+          {name}
+        </p>
+        <img src={gifUrl} className="ex-img" id={id} />
+        <div className="info-muscles" id={id}>
+          <div className="bodyPart" id={id}>
+            {bodyPart}
+          </div>
+          <div className="targetMuscles" id={id}>
+            {target}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ExerciseCards
--- src/Pages/ExercisesPage.jsx
+++ src/Pages/ExercisesPage.jsx
@@ -2,14 +2,35 @@
 import React from "react"
 import { useContext } from "react"
 import {GlobalStateContext} from "../Context/ExercisesContext.jsx";
+import ExerciseCards from "../Components/ExerciseCards/ExerciseCards.jsx"
  
+
+const exercises = [{
+bodyPart:"upper arms",
+equipment:"band",
+gifUrl:"https://api.exercisedb.io/image/irV8OHVBDHejkX",
+id:"0968",
+name:"band alternating biceps curl",
+target:"biceps",
+},
+{bodyPart:"upper arms",
+equipment:"band",
+gifUrl:"https://api.exercisedb.io/image/XX1QJqF4wWrinn",
+id:"0976",
+name:"band concentration curl",
+target:"biceps",}
+]
 
 const ExercisesPage = () => {
 
   const {muscle, setMuscle} = useContext(GlobalStateContext);
  
   return (
-    <div>{muscle}</div>
+    <div>
+     {exercises.map((item)=>{
+      <ExerciseCards bodyPart={item.bodyPart} equipment={item.equipment} gifUrl={item.gifUrl} id={item.id} name={item.name} target={item.target}  />
+     })}
+    </div>
   )
 }
 
