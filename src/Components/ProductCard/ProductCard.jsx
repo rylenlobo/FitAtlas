d9bcecf64031ec52775677ad3f59bd76ede4c371
Rylen Lobo <loborylen98334@gmail.com>
2023-09-09 22:53:41 +0530

    commit rylen

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,24 +1,11 @@
 import React from "react"
 import "./ProductCard.css"
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart"
 
-const ProductCard = ({ img, name, price, category }) => {
+const ProductCard = ({ img, name, price, category,weight }) => {
   return (
-    <div>
-      <div className="product-card">
-        <div className="product-tumb">
-          <img src={img} alt="" />
-        </div>
-        <div className="product-details">
-          <span className="product-catagory">{category}</span>
-          <h4>
-            <a href="">{name}</a>
-          </h4>
-          <div className="product-bottom-details">
-            <div className="product-price">{price}</div>
-            <div className="product-links"></div>
-          </div>
-        </div>
-      </div>
+    <div className="product">
+      
     </div>
   )
 }
